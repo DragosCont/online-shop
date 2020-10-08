fe7f8ddbe2c3e13683954ba1c9397fb53388803a
Dragos Cont <dragoscont@live.com>
2020-10-08 19:56:00 +0300

    testing in swagger ui and worked

---
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -0,0 +1,39 @@
+package org.fasttrackit.onlineshop.web;
+
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.product.ProductResponse;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody SaveProductRequest request){
+        ProductResponse product = productService.createProduct(request);
+
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id) {
+        Product product = productService.getProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+}
